37c67102121fae5ca77440061f5f93dbfb8befb9
lanlin-code <60036100+lanlin-code@users.noreply.github.com>
2020-07-14 15:51:15 +0800

    add request

--- app/src/main/java/com/example/playandroid/net/Request.java
+++ app/src/main/java/com/example/playandroid/net/Request.java
@@ -1,4 +1,49 @@
 package com.example.playandroid.net;
 
 public class Request {
+
+    private String mUrl;
+    private boolean mIsPost;
+    private String mPostData;
+
+    public String getPostData() {
+        return mPostData;
+    }
+
+    public String getUrl() {
+        return mUrl;
+    }
+
+    public boolean isPost() {
+        return mIsPost;
+    }
+
+    public static class Builder {
+        private String mUrl;
+        private boolean post = false;
+        private String postData = null;
+
+        public Builder url(String url) {
+            mUrl = url;
+            return this;
+        }
+
+        public Builder post(RequestBody body) {
+            post = true;
+            postData = body.getData();
+            return this;
+        }
+
+        public Request build() {
+            Request request = new Request();
+            request.mIsPost = post;
+            request.mUrl = mUrl;
+            request.mPostData = postData;
+            return request;
+        }
+
+
+
+    }
+
 }
