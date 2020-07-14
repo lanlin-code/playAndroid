18f43c6ecee1240f60c544eebfd401ec9f69e11f
lanlin-code <60036100+lanlin-code@users.noreply.github.com>
2020-07-14 15:53:55 +0800

    add OkHttpClient

--- app/src/main/java/com/example/playandroid/net/OkHttpClient.java
+++ app/src/main/java/com/example/playandroid/net/OkHttpClient.java
@@ -1,4 +1,14 @@
 package com.example.playandroid.net;
 
 public class OkHttpClient {
+
+    // 将数据从request对象拷贝到新创建的call对象中，返回这个call对象
+    public Call newCall(Request request) {
+        Call call = new Call();
+        call.setPost(request.isPost());
+        call.setPostData(request.getPostData());
+        call.setUrl(request.getUrl());
+        return call;
+    }
+
 }
