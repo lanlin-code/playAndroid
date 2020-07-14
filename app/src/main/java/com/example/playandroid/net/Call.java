90c7bce1f9704ca1f34465fdf6435cb204976330
lanlin-code <60036100+lanlin-code@users.noreply.github.com>
2020-07-14 15:52:37 +0800

    add Call

--- app/src/main/java/com/example/playandroid/net/Call.java
+++ app/src/main/java/com/example/playandroid/net/Call.java
@@ -1,4 +1,100 @@
 package com.example.playandroid.net;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class Call {
+
+    private String mPostData;
+    private String mUrl;
+    private boolean mPost;
+
+    void setPost(boolean mPost) {
+        this.mPost = mPost;
+    }
+
+    void setPostData(String mPostData) {
+        this.mPostData = mPostData;
+    }
+
+    void setUrl(String mUrl) {
+        this.mUrl = mUrl;
+    }
+
+    public Response execute() {
+        StringBuilder builder;
+        if (mPost) builder = post();
+        else builder = get();
+        return new Response(builder);
+    }
+
+    /**
+     * 发送post请求并读取返回数据
+     * @return 返回一个携带数据的StringBuilder
+     */
+    private StringBuilder post() {
+        HttpURLConnection connection = null;
+        DataOutputStream outputStream = null;
+        BufferedReader reader = null;
+        StringBuilder stringBuilder = new StringBuilder();;
+        try {
+            connection = (HttpURLConnection) new URL(mUrl).openConnection();
+            connection.setRequestMethod("POST");
+            outputStream = new DataOutputStream(connection.getOutputStream());
+            outputStream.writeBytes(mPostData);
+            int requestCode = connection.getResponseCode();
+            if (requestCode == HttpURLConnection.HTTP_OK) {
+                String line;
+                reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                while ((line = reader.readLine()) != null) stringBuilder.append(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (outputStream != null) {
+                try {
+                    outputStream.close();
+                    if (reader != null) reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (connection != null) connection.disconnect();
+        }
+        return stringBuilder;
+    }
+
+    /**
+     * 发送get请求并读取返回数据
+     * @return 返回携带数据的StringBuilder
+     */
+    private StringBuilder get() {
+        HttpURLConnection connection = null;
+        BufferedReader reader = null;
+        StringBuilder builder = new StringBuilder();
+        try {
+            connection = (HttpURLConnection) new URL(mUrl).openConnection();
+            connection.setRequestMethod("GET");
+            reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String line;
+            if (connection.getResponseCode() == HttpURLConnection.HTTP_OK)
+                while ((line = reader.readLine()) != null) builder.append(line);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (connection != null) connection.disconnect();
+        }
+        return builder;
+    }
 }
