3901a66e13297099f31f648bf13a6aa0a1809bf8
lanlin-code <60036100+lanlin-code@users.noreply.github.com>
2020-07-15 11:27:36 +0800

    create TextAdapter.java

---
+++ app/src/main/java/com/example/playandroid/TextAdapter.java
@@ -0,0 +1,61 @@
+package com.example.playandroid;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.playandroid.entity.Text;
+
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class TextAdapter extends RecyclerView.Adapter {
+
+    private List<Text> mList;
+
+    public TextAdapter(List<Text> list) {
+        mList = list;
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView mChapterName;
+        TextView mNiceDate;
+        TextView mTitle;
+        TextView mAuthor;
+
+
+        ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            mAuthor = itemView.findViewById(R.id.text_author);
+            mChapterName = itemView.findViewById(R.id.chapter_name);
+            mNiceDate = itemView.findViewById(R.id.nice_date);
+            mTitle.findViewById(R.id.text_title);
+        }
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.linearlayout_item_text, null);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        ViewHolder viewHolder = (ViewHolder) holder;
+        Text item = mList.get(position);
+        viewHolder.mTitle.setText(item.getTitle());
+        viewHolder.mNiceDate.setText(item.getNiceDate());
+        viewHolder.mAuthor.setText(item.getAuthor());
+        viewHolder.mChapterName.setText(item.getSuperChapterName() + "/" + item.getChapterName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mList.size();
+    }
+}
