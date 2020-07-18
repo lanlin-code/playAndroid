8090e3d9c829f31b0bc4d0c1e4263a59078596e0
lanlin-code <60036100+lanlin-code@users.noreply.github.com>
2020-07-18 14:53:56 +0800

    create WebActivity

--- app/src/main/java/com/example/playandroid/view/WebActivity.java
+++ app/src/main/java/com/example/playandroid/view/WebActivity.java
@@ -1,13 +1,102 @@
 package com.example.playandroid.view;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ImageButton;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.playandroid.R;
+import com.example.playandroid.manager.DataTransferManager;
 
 public class WebActivity extends AppCompatActivity {
+
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) actionBar.hide();
+
+        final TextView webTitle = findViewById(R.id.web_title);
+        ImageButton back = findViewById(R.id.web_back);
+        final ProgressBar progressBar = findViewById(R.id.web_progress);
+        webView = findViewById(R.id.webView);
+
+        Intent intent = getIntent();
+        String link = intent.getStringExtra(DataTransferManager.KEY);
+
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                progressBar.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                progressBar.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+                super.onReceivedError(view, request, error);
+                Toast.makeText(WebActivity.this, "加载失败", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                super.onProgressChanged(view, newProgress);
+                progressBar.setProgress(newProgress);
+            }
+
+            @Override
+            public void onReceivedTitle(WebView view, String title) {
+                super.onReceivedTitle(view, title);
+                webTitle.setText(title);
+            }
+        });
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        webView.loadUrl(link);
+
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        webView.loadUrl(null);
+        ViewGroup parent = (ViewGroup) webView.getParent();
+        parent.removeView(webView);
+        webView.destroy();
+        webView = null;
     }
 }
